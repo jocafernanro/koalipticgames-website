58ad9b42503cc04545533ee4e0c0c9945d33467b
José Carlos Fernández Rodríguez <jcfernandez@Joses-MacBook-Pro.local>
2022-06-15 21:33:46 +0200

    project setup and a basic home page

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Home from './views/Home.vue'
+</script>
+
+<template>
+  <Home />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  background-color: #D67073;
+  height: 100vh;
+  width: 100%;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-family: Staatliches, Arial, Helvetica, sans-serif;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+    <div class="md:w-2/3 lg:w-1/2 flex flex-col items-center">
+        <h1 class="text-4xl font-bold">Making super kind games</h1>
+        <img src="../assets/images/Logo_White.png" alt="logo">
+        <h2 class="text-4xl font-bold">Coming Soon. Expect the jungle!</h2>
+    </div>
+</template>
+
+
+<style scoped>
+</style>
